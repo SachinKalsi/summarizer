8ea1b958f5217e30d0efdfd4cbd0e8af0bd833c0
Eric Bower <neurosnap@gmail.com>
2015-08-25 16:54:30 -0400

    getting there

--- tests/test_summarizer.py
+++ tests/test_summarizer.py
@@ -4,10 +4,11 @@ import unittest
 
 from summarizer import summarize
 
+_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
+
 class TestSummaries(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        _dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
         test_files = ['autotech.txt', 'misty.txt', 'morouns.txt']
 
         cls.autotech = {}
@@ -17,9 +18,13 @@ class TestSummaries(unittest.TestCase):
             cls.autotech['text'] = "\n".join(lines[1:])
 
     def test_autotech_summary(self):
-        summary = summarize(self.autotech['title'], self.autotech['text'])
-        for sentence in summary:
-            self.assertEqual(sentence, sentence)
+        expected_summary = None
+        with open(os.path.join(_dir, 'autotech_s.txt'), 'r') as fp:
+            expected_summary = fp.readlines()
+        actual_summary = summarize(self.autotech['title'], self.autotech['text'])
+
+        for actual, expected in zip(actual_summary, expected_summary):
+            self.assertEqual(actual.replace('\n', ''), expected.replace('\n', ''))
 
 if __name__ == '__main__':
     unittest.run(verbosity=2)
