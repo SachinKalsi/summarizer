e4eaf51d230d460b6541c41efb2843c475085867
Eric Bower <neurosnap@gmail.com>
2015-08-25 16:23:10 -0400

    removed swp, quick n dirty cli

---
+++ cli.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import sys
+from summarizer import summarize
+
+lines = sys.stdin.readlines()
+summary = summarize(lines[0], "\n".join(lines[1:]))
+for sentence in summary:
+    print(sentence)
--- test.py
+++
@@ -1,3 +0,0 @@
-from summarizer import summarize
-
-print(summarize("a title", "this is one sentence. This is another sentence"))
