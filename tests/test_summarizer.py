e158b4f69408e8601da4231f7b4ce486971e0a08
Eric Bower <neurosnap@gmail.com>
2015-08-26 14:05:55 -0400

    py2 and py3 compat

--- tests/test_summarizer.py
+++ tests/test_summarizer.py
@@ -12,6 +12,16 @@ def _read_expected_summary(fname, _dir=_dir):
         expected_summary = fp.readlines()
     return expected_summary
 
+def _clean_sentence(s):
+    return _u(s.replace('\n', ''))
+
+def _u(s):
+    _str = s
+    try:
+        _str = _str.decode('utf-8', 'ignore')
+    except AttributeError:
+        pass
+    return _str
 
 class TestSummaries(unittest.TestCase):
     @classmethod
@@ -21,41 +31,41 @@ class TestSummaries(unittest.TestCase):
         cls.autotech = {}
         with open(os.path.join(_dir, 'autotech.txt'), 'r') as fp:
             lines = fp.readlines()
-            cls.autotech['title'] = lines[0]
-            cls.autotech['text'] = "".join(lines[1:])
+            cls.autotech['title'] = _u(lines[0])
+            cls.autotech['text'] = _u("".join(lines[1:]))
 
         cls.misty = {}
         with open(os.path.join(_dir, 'misty.txt'), 'r') as fp:
             lines = fp.readlines()
-            cls.misty['title'] = lines[0]
-            cls.misty['text'] = "".join(lines[1:])
+            cls.misty['title'] = _u(lines[0])
+            cls.misty['text'] = _u("".join(lines[1:]))
 
         cls.morouns = {}
         with open(os.path.join(_dir, 'morouns.txt'), 'r') as fp:
             lines = fp.readlines()
-            cls.morouns['title'] = lines[0]
-            cls.morouns['text'] = "".join(lines[1:])
+            cls.morouns['title'] = _u(lines[0])
+            cls.morouns['text'] = _u("".join(lines[1:]))
 
     def test_autotech_summary(self):
         expected_summary = _read_expected_summary('autotech_s.txt')
         actual_summary = summarize(self.autotech['title'], self.autotech['text'])
 
         for actual, expected in zip(actual_summary, expected_summary):
-            self.assertEqual(actual.replace('\n', ''), expected.replace('\n', ''))
+            self.assertEqual(actual.replace('\n', ''), _clean_sentence(expected))
 
     def test_misty_summary(self):
         expected_summary = _read_expected_summary('misty_s.txt')
         actual_summary = summarize(self.misty['title'], self.misty['text'])
 
         for actual, expected in zip(actual_summary, expected_summary):
-            self.assertEqual(actual.replace('\n', ''), expected.replace('\n', ''))
+            self.assertEqual(actual.replace('\n', ''), _clean_sentence(expected))
 
     def test_morouns_summary(self):
         expected_summary = _read_expected_summary('morouns_s.txt')
         actual_summary = summarize(self.morouns['title'], self.morouns['text'])
 
         for actual, expected in zip(actual_summary, expected_summary):
-            self.assertEqual(actual.replace('\n', ''), expected.replace('\n', ''))
+            self.assertEqual(actual.replace('\n', ''), _clean_sentence(expected))
 
 if __name__ == '__main__':
     unittest.run(verbosity=2)
