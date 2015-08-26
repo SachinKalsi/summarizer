aa59447eef76c92b5762a860d8602556a16f2447
Eric Bower <neurosnap@gmail.com>
2015-08-26 14:09:32 -0400

    removed newline join

--- cli.py
+++ cli.py
@@ -3,6 +3,6 @@ import sys
 from summarizer import summarize
 
 lines = sys.stdin.readlines()
-summary = summarize(lines[0], "\n".join(lines[1:]))
+summary = summarize(lines[0], "".join(lines[1:]))
 for sentence in summary:
     print(sentence.replace("\n", ""))
