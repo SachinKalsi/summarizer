6a30f7177ed657e4a1b562472a1c3dc6aae9430d
Eric Bower <neurosnap@gmail.com>
2015-10-01 12:40:40 -0400

    version bump because pypi is not ingesting my json file

--- summarizer/__init__.py
+++ summarizer/__init__.py
@@ -2,7 +2,7 @@
 from .parser import Parser
 from .summarizer import Summarizer
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 def summarize(title, text, count=3, summarizer=None):
     if not summarizer:
